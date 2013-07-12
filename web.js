66f9d685d1fc7e175beee5d113a34462356a92d4
Antony N <kariuki.a@gmail.com>
2013-07-12 13:14:04 +0000

    edited web.js

--- web.js
+++ web.js
@@ -1,13 +1,23 @@
-var express = require('express');
+//var express = require('express');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+//app.get('/', function(request, response) {
 
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+//});
+
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
+
+var content;
+
+fs.readFileSync('./Index.html', function read(err, data) {
+   if (err) {
+      throw err;
+   }
+   content = data;
 });
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+console.log(content);
