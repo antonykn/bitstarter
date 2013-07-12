4e8b164b4b5b6f2725c65ae46218930969e8dbbf
Antony N <kariuki.a@gmail.com>
2013-07-12 22:58:03 +0000

    edited web.js

--- web.js
+++ web.js
@@ -13,11 +13,18 @@
 //});
 
 var content;
+// First I want to read the file
+fs.readFile('./index.html', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
 
-fs.readFileSync('./Index.html', function read(err, data) {
-   if (err) {
-      throw err;
-   }
-   content = data;
+    // Invoke the next step here however you like
+    console.log(content);   // Put all of the code here (not the best solution)
+    processFile();          // Or put the next step in a function and invoke it
 });
-console.log(content);
+
+function processFile() {
+    console.log(content);
+}
