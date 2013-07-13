d06a0d0d962ac64d7f28ae187b006b5221685634
Antony N <kariuki.a@gmail.com>
2013-07-13 16:17:28 +0000

    edited web.js

--- web.js
+++ web.js
@@ -3,14 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    // set up file system;
-    var fs = require('fs'); 
-
-    // read index.html into buffer;  
-    var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8');
-    response.send(buffer.toString());
+	var fs = require('fs');
+	var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8');
+	response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
-app.listen( console.log("Listening on " + port);
-});port, function() {
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
